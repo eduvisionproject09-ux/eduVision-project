f17bd97fbad2c51a828189808b091a60071720a9
nehal <nhbhuiyan2003@gmail.com>
2026-04-29 20:48:03 +0600

    initial commit

---
+++ eduvisionbackend/src/main/java/com/academicproject/eduvisionbackend/EduvisionbackendApplication.java
@@ -0,0 +1,13 @@
+package com.academicproject.eduvisionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduvisionbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduvisionbackendApplication.class, args);
+	}
+
+}
---
+++ eduvisionbackend/src/test/java/com/academicproject/eduvisionbackend/EduvisionbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academicproject.eduvisionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduvisionbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
